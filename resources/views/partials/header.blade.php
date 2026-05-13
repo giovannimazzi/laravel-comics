74bccd6284a311dfd6cd6e128ef0469fb3dc9a74
giovannimazzi <gioelor@libero.it>
2026-05-13 22:06:18 +0200

    add navbar features and details

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,27 +1,35 @@
 <header class="bg-white">
-    <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg bg-white">
         <div class="container">
-            <a class="navbar-brand" href="/">
+            <a class="navbar-brand" href="{{ route('comics.index') }}">
                 <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo" width="80">
             </a>
 
-            <ul class="navbar-nav ms-auto gap-3">
-                <li class="nav-item">
-                    <a class="nav-link text-uppercase fw-bold" href="#">Characters</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-uppercase fw-bold active" href="#">Comics</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-uppercase fw-bold" href="#">Movies</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-uppercase fw-bold" href="#">Tv</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-uppercase fw-bold" href="#">Games</a>
-                </li>
-            </ul>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="mainNavbar">
+                <ul class="navbar-nav ms-auto gap-lg-3">
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase fw-bold {{ request()->routeIs('characters') ? 'active' : '' }}" href="{{ route('characters') }}">
+                            Characters
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase fw-bold {{ request()->routeIs('comics.*') ? 'active' : '' }}" href="{{ route('comics.index') }}">
+                            Comics
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase fw-bold {{ request()->routeIs('movies') ? 'active' : '' }}" href="{{ route('movies') }}">
+                            Movies
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 </header>
