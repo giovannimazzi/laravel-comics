73b83684e4d4cabac6df972dcab08e8bb0a129df
giovannimazzi <gioelor@libero.it>
2026-05-13 22:06:38 +0200

    add some routes

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,12 @@ Route::get('/', function () {
     $comics = config('comics');
 
     return view('comics.index', compact('comics'));
-});
+})->name('comics.index');
+
+Route::get('/characters', function () {
+    return view('pages.characters');
+})->name('characters');
+
+Route::get('/movies', function () {
+    return view('pages.movies');
+})->name('movies');
