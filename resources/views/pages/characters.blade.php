bc1ec8fda1efc1a80b60c1997d935e85052fd522
giovannimazzi <gioelor@libero.it>
2026-05-13 22:05:50 +0200

    add characters and movies pages

---
+++ resources/views/pages/characters.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Characters</h1>
+
+@endsection
---
+++ resources/views/pages/movies.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Movies</h1>
+
+@endsection
